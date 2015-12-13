7b986808cc938f1844009a71b024d6a7d0b5a3ab
Chris Fortier <cfortier2@gmail.com>
2015-12-12 23:45:02 -0500

    hook up databases

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,7 @@ RUN apt-get update && \
     libpng12-dev \
     libbz2-dev \
     php-pear \
+    php5-mysqlnd \
     curl \
     git \
     vim \
@@ -22,16 +23,15 @@ RUN apt-get update && \
 # PHP Extensions
 RUN docker-php-ext-install mcrypt zip bz2 mbstring \
   && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
-  && docker-php-ext-install gd
+  && docker-php-ext-install gd \
+  && docker-php-ext-install opcache \
+  && docker-php-ext-install mysql mysqli pdo pdo_mysql
 
 # install composer
 RUN curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer
 
 # move application to correct location
 COPY . /app
-
-# enable opcache
-RUN php5enmod opcache
 
 # set apache conf
 COPY ./etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,3 +20,39 @@ Route::resource('/', 'IndexController');
 Route::get('/phpinfo', function () {
   return phpinfo();
 });
+
+// debug route
+Route::get('/debug', function() {
+
+    echo '<pre>';
+
+    echo '<h1>Environment</h1>';
+    echo App::environment().'</h1>';
+
+    echo '<h1>Debugging?</h1>';
+    if(config('app.debug')) echo "Yes"; else echo "No";
+
+    echo '<h1>Database Config</h1>';
+    /*
+    The following line will output your MySQL credentials.
+    Uncomment it only if you're having a hard time connecting to the database and you
+    need to confirm your credentials.
+    When you're done debugging, comment it back out so you don't accidentally leave it
+    running on your live server, making your credentials public.
+    */
+    //print_r(config('database.connections.mysql'));
+
+    echo '<h1>Test Database Connection</h1>';
+    try {
+        $results = DB::select('SHOW DATABASES;');
+        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+        echo "<br><br>Your Databases:<br><br>";
+        print_r($results);
+    }
+    catch (Exception $e) {
+        echo '<strong style="background-color:crimson; ">Caught exception: ', $e, "</strong>\n";
+    }
+
+    echo '</pre>';
+
+});
