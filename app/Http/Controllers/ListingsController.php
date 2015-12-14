eb8dfa6035a78aeab7f3ed6aa5c9aea16cce19f7
Chris Fortier <cfortier2@gmail.com>
2015-12-13 20:05:15 -0500

    second return listings

--- app/Http/Controllers/ListingsController.php
+++ app/Http/Controllers/ListingsController.php
@@ -26,6 +26,15 @@ class ListingsController extends Controller
             'type' => 'Estate',
             'bedrooms' => 15,
             'image' => 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+            ],
+            [
+            'id' => 2,
+            'title' => 'Urban Living',
+            'owner' => 'Mike TV',
+            'city' => 'Seattle',
+            'type' => 'Condo',
+            'bedrooms' => 1,
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
           ]
         ]
       );
