eb75fc0c602cb1a150ce50b7d29aba6fe082dbe5
Chris Fortier <cfortier2@gmail.com>
2015-12-21 02:30:15 -0500

    cleanup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,37 +26,37 @@ Route::get('/phpinfo', function () {
 });
 
 // debug route
-Route::get('/debug', function() {
+/* Route::get('/debug', function() { */
 
-    echo '<pre>';
+/*     echo '<pre>'; */
 
-    echo '<h1>Environment</h1>';
-    echo App::environment().'</h1>';
+/*     echo '<h1>Environment</h1>'; */
+/*     echo App::environment().'</h1>'; */
 
-    echo '<h1>Debugging?</h1>';
-    if(config('app.debug')) echo "Yes"; else echo "No";
+/*     echo '<h1>Debugging?</h1>'; */
+/*     if(config('app.debug')) echo "Yes"; else echo "No"; */
 
-    echo '<h1>Database Config</h1>';
-    /*
-    The following line will output your MySQL credentials.
-    Uncomment it only if you're having a hard time connecting to the database and you
-    need to confirm your credentials.
-    When you're done debugging, comment it back out so you don't accidentally leave it
-    running on your live server, making your credentials public.
-    */
-    //print_r(config('database.connections.mysql'));
+/*     echo '<h1>Database Config</h1>'; */
+/*     /* */
+/*     The following line will output your MySQL credentials. */
+/*     Uncomment it only if you're having a hard time connecting to the database and you */
+/*     need to confirm your credentials. */
+/*     When you're done debugging, comment it back out so you don't accidentally leave it */
+/*     running on your live server, making your credentials public. */
+/*     *1/ */
+/*     //print_r(config('database.connections.mysql')); */
 
-    echo '<h1>Test Database Connection</h1>';
-    try {
-        $results = DB::select('SHOW DATABASES;');
-        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-        echo "<br><br>Your Databases:<br><br>";
-        print_r($results);
-    }
-    catch (Exception $e) {
-        echo '<strong style="background-color:crimson; ">Caught exception: ', $e, "</strong>\n";
-    }
+/*     echo '<h1>Test Database Connection</h1>'; */
+/*     try { */
+/*         $results = DB::select('SHOW DATABASES;'); */
+/*         echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>'; */
+/*         echo "<br><br>Your Databases:<br><br>"; */
+/*         print_r($results); */
+/*     } */
+/*     catch (Exception $e) { */
+/*         echo '<strong style="background-color:crimson; ">Caught exception: ', $e, "</strong>\n"; */
+/*     } */
 
-    echo '</pre>';
+/*     echo '</pre>'; */
 
-});
+/* }); */
